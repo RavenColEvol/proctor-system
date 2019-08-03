1435b2bae0d1f0530ef125ae2c5d250418a662ab
ravi <ravilamkoti@gmail.com>
2019-08-03 20:01:06 +0530

    v0.1Added pages and little bit of functionality

--- proctorForm/proctorForm/settings.py
+++ proctorForm/proctorForm/settings.py
@@ -83,12 +83,12 @@ WSGI_APPLICATION = 'proctorForm.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'postgres',
         'USER': 'postgres',
-        'PASSWORD': 'admin',
+        'PASSWORD': 'nontoxic',
         'HOST': 'localhost',
-        'PORT': '5432',
+        'PORT': '',
     }
 }
 
@@ -131,6 +131,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
--- proctorForm/src/views.py
+++ proctorForm/src/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView,UpdateView
+from src.forms import ProctorForm
+from src.models import Student
 # Create your views here.
 def index(request):
 	return render(request,'src/index.html',{})
+
+def student_view(request):
+	return render(request,'src/student.html',{})
+
+def proctor_view(request):
+	return render(request,'src/proctor.html',{})
+
+def department_view(request):
+	return render(request,'src/department.html',{})
+
+def student_form(request):
+	form = ProctorForm()
+	return render(request,'src/forms/student_form.html',{'form':form})
+
+class StudentForm(CreateView):
+	model = Student
+	fields = '__all__'
+
+class StudentUpdateForm(UpdateView):
+	model = Student
+	fields = '__all__'
+	template_name_suffix = '_update_form'
