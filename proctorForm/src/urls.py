05a24d2a5be2cab2cd6203f1d673d8d14910f9c0
ravi <ravilamkoti@gmail.com>
2019-08-04 11:29:35 +0530

    v0.2

--- proctorForm/src/urls.py
+++ proctorForm/src/urls.py
@@ -5,8 +5,8 @@ from src import views
 urlpatterns = [
 	path('',views.index,name='home'),
 	path('department/',views.department_view,name='department_view'),
-	path('proctor/',views.department_view,name='proctor_view'),
-	path('student/',views.department_view,name='student_view'),
+	path('proctor/',views.proctor_view,name='proctor_view'),
+	path('student/',views.student_view,name='student_view'),
 	path('proctor_form/',views.StudentForm.as_view(),name='proctor_form'),
-	path('proctor_update_form/<int:pk>',views.StudentUpdateForm.as_view(),name='proctor_update_form'),
+	#path('proctor_update_form/<int:pk>',views.StudentUpdateForm.as_view(),name='proctor_update_form'),
 ]
