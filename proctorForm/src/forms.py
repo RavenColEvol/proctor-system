0db75febe269dfbf49efaf436624fdd40fc58d31
ravi <ravilamkoti@gmail.com>
2019-08-05 16:00:53 +0530

    v0.3 Added student login and pages

--- proctorForm/src/forms.py
+++ proctorForm/src/forms.py
@@ -1,8 +1,22 @@
 from django import forms
-from src.models import Student
-
+from django.contrib.auth.forms import UserCreationForm
+from src.models import Student, User
+from django.db import transaction
 
 class ProctorForm(forms.ModelForm):
 	class Meta:
 		model = Student
-		fields = '__all__'
+		exclude = ['user',]
+
+class StudentSignUpForm(UserCreationForm):	
+	class Meta(UserCreationForm.Meta):
+		model=User
+
+	@transaction.atomic
+	def save(self):
+		user = super().save(commit=False)
+		user.is_student = True
+		user.save()
+		return user
+
+
--- proctorForm/src/urls.py
+++ proctorForm/src/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from src import views
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
 	path('',views.index,name='home'),
@@ -8,5 +8,10 @@ urlpatterns = [
 	path('proctor/',views.proctor_view,name='proctor_view'),
 	path('student/',views.student_view,name='student_view'),
 	path('proctor_form/',views.StudentForm.as_view(),name='proctor_form'),
+	path('student_signup_form/',views.StudentSignUp.as_view(),name='student_signup'),
+
+    #authentication
+    path('login/',auth_views.LoginView.as_view(),name='login'),
+    path('logout/',views.logout_user,name='logout')
 	#path('proctor_update_form/<int:pk>',views.StudentUpdateForm.as_view(),name='proctor_update_form'),
 ]
